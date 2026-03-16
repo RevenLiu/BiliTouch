4c0f0f0502281be3833c94fd17f0a8e18bc21ecf
Reven <k3477a@gmail.com>
2026-03-16 22:06:08 +0800

    v1.1.2
    - 修复了直播播放器刷新后亮度调节层被覆盖的问题

--- BiliTouch.js
+++ BiliTouch.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         BiliTouch
 // @namespace    https://github.com/RevenLiu
-// @version      1.1.1
+// @version      1.1.2
 // @description  一个为移动端打造的Web端B站网页播放器交互重构的篡改猴脚本。支持两侧滑动调节亮度与音量、横向滑动调节时间进度、单击显隐工具栏、双击播放/暂停、双指缩放位移、长按倍速。让网页版拥有原生 App 般的使用体验。
 // @author       RevenLiu
 // @license      MIT
@@ -36,7 +36,7 @@
         }
         #brightness-overlay {
             position: absolute; top: 0; left: 0; width: 100%; height: 100%;
-            background: black; opacity: 0; pointer-events: none; z-index: 1;
+            background: black; opacity: 0; pointer-events: none; z-index: 1000000;
         }
         .bpx-player-contextmenu.bpx-player-active { display: none !important; visibility: hidden !important; opacity: 0 !important; }
     `;
